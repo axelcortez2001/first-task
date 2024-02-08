e2d25eb003ba33f6c11822cc3df65d06018a1502
Axel <johnaxelcortez2001@gmail.com>
2024-02-08 20:07:56 +0800

    fixed cors error "/"

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://master.d2jxnxewppnx52.amplifyapp.com/" }, 
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://master.d2jxnxewppnx52.amplifyapp.com",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
